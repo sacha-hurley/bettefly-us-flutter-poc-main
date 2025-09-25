831eb1a13e45210e47afd9c291a3034b9c969ed1
Sacha <sachahurley@gmail.com>
2025-09-24 19:47:04 -0700

    chore(pages): remove no-op fetch handler from SW

--- .gh-pages-worktree/preview-318fea2/flutter_service_worker.js
+++ .gh-pages-worktree/preview-318fea2/flutter_service_worker.js
@@ -1,3 +1,2 @@
 self.addEventListener("install",(e)=>{self.skipWaiting();});
 self.addEventListener("activate",(event)=>{event.waitUntil((async()=>{try{await self.clients.claim();await self.registration.unregister();}catch(e){}})());});
-self.addEventListener("fetch",(e)=>{});
