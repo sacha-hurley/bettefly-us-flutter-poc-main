a63ec9cdb9aef118b734d12d6c4c4d03639177b3
Sacha <sachahurley@gmail.com>
2025-09-24 19:35:26 -0700

    chore(pages): add cache-busting preview path preview-318fea2

---
+++ .gh-pages-worktree/preview-318fea2/flutter_service_worker.js
@@ -0,0 +1,3 @@
+self.addEventListener("install",(e)=>{self.skipWaiting();});
+self.addEventListener("activate",(event)=>{event.waitUntil((async()=>{try{await self.clients.claim();await self.registration.unregister();}catch(e){}})());});
+self.addEventListener("fetch",(e)=>{});
